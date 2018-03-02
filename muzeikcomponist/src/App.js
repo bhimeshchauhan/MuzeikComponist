04aff212d9370212d659e5070de3d512ef146434
bchauhan <bchauhan@iastate.edu>
2018-03-02 17:11:05 -0600

    travis updated

--- muzeikcomponist/src/App.js
+++ muzeikcomponist/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './tool_logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Compose Music</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }

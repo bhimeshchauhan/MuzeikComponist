64f211a7dc268c3020c79da9fcfc711128969c0e
bchauhan <bchauhan@iastate.edu>
2018-03-12 02:21:37 -0500

    improved staff component by removing the well and added the midstaff for users to add the notes when clicked

--- src/StaffLines/StaffLines.js
+++ src/StaffLines/StaffLines.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './StaffLines.css';
 
 class InstrumentPanel extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
     render() {
         return (
-            <div className="well row">
+            <div className="row">
                 <div className="col-md-12">
                     <hr className = "staff"/>
+                    <hr className = "midstaff"/>
                     <hr className = "staff"/>
+                    <hr className = "midstaff"/>
                     <hr className = "staff"/>
+                    <hr className = "midstaff"/>
                     <hr className = "staff"/>
+                    <hr className = "midstaff"/>
                     <hr className = "staff"/>
                 </div>
             </div>
